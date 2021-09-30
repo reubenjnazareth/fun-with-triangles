95b2a9b7fbff74dcf9d67b27418981603fcd84ff
rnaz25 <rnaz25@gmail.com>
2021-09-30 22:53:06 +0530

    update

--- areaOfTriangle.js
+++ areaOfTriangle.js
@@ -1,3 +1,12 @@
 const input = document.querySelectorAll(".input");
 const calBtn= document.querySelector("#calBtn");
-const output = document.querySelector("#output");
+const output = document.querySelector("#output");\
+
+function calArea(){
+   
+    
+    const areaOfTriangle = (Number(input[0].value) * Number(input[1].value)) / 2;
+    output.innerText = "Area of Triangle from Given Input: " + areaOfTriangle;
+}
+
+calBtn.addEventListener("click", calArea)
