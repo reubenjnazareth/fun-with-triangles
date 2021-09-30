628d424ece39db377e3398e9a37ddd5cfa1d5bfa
rnaz25 <rnaz25@gmail.com>
2021-09-30 22:56:57 +0530

    Create hypoteneuse.js

---
+++ hypoteneuse.js
@@ -0,0 +1,9 @@
+const sides = document.querySelectorAll(".input-side");
+const hypBtn = document.querySelector("#calculate-Btn");
+const output = document.querySelector("#output");
+
+
+
+
+
+hypBtn.addEventListener("click", calHypoteneuse)
