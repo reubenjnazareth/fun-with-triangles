07ed42ca6455cd6735f0fd7a52d50cee2b0d0dad
rnaz25 <rnaz25@gmail.com>
2021-09-30 22:57:22 +0530

    update

--- hypoteneuse.js
+++ hypoteneuse.js
@@ -2,7 +2,16 @@ const sides = document.querySelectorAll(".input-side");
 const hypBtn = document.querySelector("#calculate-Btn");
 const output = document.querySelector("#output");
 
+function calSumOfSquares(a,b) {
+    const sumOfSquares= a*a + b*b;
+    return sumOfSquares
+}
 
+function calHypoteneuse() {
+    const sumOfSquares = calSumOfSquares(Number(sides[0].value), Number(sides[1].value));
+    const lengthOfHyp = Math.sqrt(sumOfSquares);
+    output.innerText = "Length of Hypoteneuse: " + lengthOfHyp
+}
 
 
 
